f39db56e5bd71f1458fb8ac5b594b74a633032e6
Manuel Krebs <me@leun4m.de>
2020-12-19 00:14:02 +0100

    2020 / Day 15 (2)

--- src/year_2020/day_15.rs
+++ src/year_2020/day_15.rs
@@ -1,13 +1,12 @@
 use std::collections::HashMap;
 
-const NUMBER_OF_INTEREST: usize = 2020;
-
 pub fn main(input: &str) {
     let numbers: Vec<usize> = input.split(',').map(|x| x.parse().unwrap()).collect();
-    println!("Part 1: {}", play(&numbers));
+    println!("Part 1: {}", play(&numbers, 2020));
+    println!("Part 1: {}", play(&numbers, 30000000));
 }
 
-fn play(start: &[usize]) -> usize {
+fn play(start: &[usize], number_of_interest: usize) -> usize {
     let mut numbers = HashMap::new();
     for (i, num) in start.iter().enumerate() {
         numbers.insert(*num, i + 1);
@@ -15,7 +14,7 @@ fn play(start: &[usize]) -> usize {
 
     let mut previous = *start.iter().last().expect("List must not be empty!");
     let mut pre_previous;
-    for n in start.len()..NUMBER_OF_INTEREST {
+    for n in start.len()..number_of_interest {
         pre_previous = previous;
         previous = if let Some(x) = numbers.get(&previous) {
             n - x
@@ -23,7 +22,6 @@ fn play(start: &[usize]) -> usize {
             0
         };
         numbers.insert(pre_previous, n);
-        // println!("{}:\t{} {:?}", n + 1, previous, numbers);
     }
 
     previous
@@ -35,11 +33,13 @@ mod test {
 
     #[test]
     fn example() {
-        assert_eq!(1, play(&[1, 3, 2]));
-        assert_eq!(10, play(&[2, 1, 3]));
-        assert_eq!(27, play(&[1, 2, 3]));
-        assert_eq!(78, play(&[2, 3, 1]));
-        assert_eq!(438, play(&[3, 2, 1]));
-        assert_eq!(1836, play(&[3, 1, 2]));
+        const FIRST: usize = 2020;
+
+        assert_eq!(1, play(&[1, 3, 2], FIRST));
+        assert_eq!(10, play(&[2, 1, 3], FIRST));
+        assert_eq!(27, play(&[1, 2, 3], FIRST));
+        assert_eq!(78, play(&[2, 3, 1], FIRST));
+        assert_eq!(438, play(&[3, 2, 1], FIRST));
+        assert_eq!(1836, play(&[3, 1, 2], FIRST));
     }
 }
