ec7badd31efe518e02f6ada4c52348bafa588e4f
Manuel Krebs <me@leun4m.de>
2021-11-13 12:04:00 +0100

    2018 / Day 01 (2)

--- src/year_2018/day_01.rs
+++ src/year_2018/day_01.rs
@@ -1,18 +1,42 @@
+use std::collections::HashSet;
+
 pub fn main(input: &str) {
     let numbers = parse(input);
     println!("Part 1: {}", part_one(&numbers));
+    println!("Part 2: {}", part_two(&numbers));
 }
 
 fn parse(input: &str) -> Vec<i32> {
-    input.lines().filter(|line| !line.is_empty()).map(|line| parse_line(line)).collect()
+    input
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| parse_line(line))
+        .collect()
 }
 
 fn parse_line(line: &str) -> i32 {
-    line.parse().expect(&format!("Unexpected number: {}", line))
+    line.parse().unwrap()
 }
 
 fn part_one(numbers: &[i32]) -> i32 {
     numbers.iter().sum()
+}
+
+fn part_two(numbers: &[i32]) -> i32 {
+    let mut set = HashSet::from([0]);
+    let mut sum = 0;
+
+    loop {
+        for i in numbers {
+            sum += i;
+
+            if set.contains(&sum) {
+                return sum;
+            }
+
+            set.insert(sum);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -36,5 +60,13 @@ mod test {
         assert_eq!(part_one(&[1, 1, 1]), 3);
         assert_eq!(part_one(&[1, 1, -2]), 0);
         assert_eq!(part_one(&[-1, -2, -3]), -6);
+    }
+
+    #[test]
+    fn part_two_works() {
+        assert_eq!(part_two(&[1, -1]), 0);
+        assert_eq!(part_two(&[3, 3, 4, -2, -4]), 10);
+        assert_eq!(part_two(&[-6, 3, 8, 5, -6]), 5);
+        assert_eq!(part_two(&[7, 7, -2, -7, -4]), 14);
     }
 }
