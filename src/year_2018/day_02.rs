d06433dbadc0c6d23f8d9d944e9efb598cc25960
Manuel Krebs <me@leun4m.de>
2021-11-13 14:47:53 +0100

    2018 / Day 02 (2)

--- src/year_2018/day_02.rs
+++ src/year_2018/day_02.rs
@@ -2,7 +2,9 @@ use crate::util;
 
 pub fn main(input: &str) {
     let multiples = parse(input);
+    let lines = input.lines().collect::<Vec<&str>>();
     println!("Part 1: {}", part_one(&multiples));
+    println!("Part 2: {}", part_two(&lines));
 }
 
 fn parse(input: &str) -> Vec<Multiples> {
@@ -28,6 +30,45 @@ fn part_one(multiples: &[Multiples]) -> u32 {
         .reduce(|a, b| (a.0 + b.0, a.1 + b.1))
         .expect("Multiples not empty");
     twice * trice
+}
+
+fn part_two(words: &[&str]) -> String {
+    for a in words {
+        for b in words.iter().skip(1) {
+            if let Some(x) = diff_by_one(a, b) {
+                return x;
+            }
+        }
+    }
+
+    panic!("Could not find a match!");
+}
+
+fn diff_by_one(word_a: &str, word_b: &str) -> Option<String> {
+    if word_a.len() != word_b.len() {
+        return None;
+    }
+
+    let mut common_part = String::new();
+    let mut difference = 0;
+
+    for (a_char, b_char) in word_a.chars().zip(word_b.chars()) {
+        if a_char == b_char {
+            common_part.push(a_char);
+        } else {
+            difference += 1;
+        }
+
+        if difference > 1 {
+            break;
+        }
+    }
+
+    if difference == 1 {
+        Some(common_part)
+    } else {
+        None
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -76,5 +117,27 @@ mod test {
             Multiples::new(false, true),
         ];
         assert_eq!(part_one(&input), 12);
+    }
+
+    #[test]
+    fn part_two_works() {
+        let input = vec![
+            "abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz",
+        ];
+        assert_eq!(part_two(&input), "fgij");
+    }
+
+    #[test]
+    fn diff_by_one_works() {
+        assert_eq!(diff_by_one("", ""), None);
+        assert_eq!(diff_by_one("", "b"), None);
+        assert_eq!(diff_by_one("a", "ab"), None);
+        assert_eq!(diff_by_one("aby", "zbx"), None);
+
+        assert_eq!(diff_by_one("a", "b"), Some(String::new()));
+        assert_eq!(diff_by_one("ab", "ac"), Some("a".to_string()));
+        assert_eq!(diff_by_one("abf", "acf"), Some("af".to_string()));
+        assert_eq!(diff_by_one("cfb", "cfa"), Some("cf".to_string()));
+        assert_eq!(diff_by_one("abc", "zbc"), Some("bc".to_string()));
     }
 }
