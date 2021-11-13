28e3088c8705441d9224b8dc8a094a78670ba75a
Manuel Krebs <me@leun4m.de>
2021-11-13 18:23:48 +0100

    2015 / Day 13 - Parsing

---
+++ src/year_2015/day_13.rs
@@ -0,0 +1,98 @@
+use regex::Regex;
+use std::collections::HashMap;
+
+pub fn main(input: &str) {
+    let rules = parse(input);
+    println!("Part 1: {}", part_one(&rules));
+}
+
+fn parse(input: &str) -> Rules {
+    let mut rules = Rules::new();
+    for line in input.lines().filter(|l| !l.is_empty()) {
+        let (from, to, happiness) = parse_rule(line);
+        rules.add((&from, &to, happiness));
+    }
+
+    rules
+}
+
+fn parse_rule(line: &str) -> (String, String, i32) {
+    // Alice would gain 54 happiness units by sitting next to Bob.
+    let regex =
+        Regex::new(r"(\w+) would (\w+) (\d+) happiness units by sitting next to (\w+).").unwrap();
+    let capture = regex.captures(line).expect("Looks weird");
+
+    let from = capture[1].parse::<String>().unwrap();
+    let gain = capture[2].parse::<String>().unwrap();
+    let factor = if gain == "gain" { 1 } else { -1 };
+    let happiness = factor * capture[3].parse::<i32>().unwrap();
+    let to = capture[4].parse::<String>().unwrap();
+    (from, to, happiness)
+}
+
+fn part_one(rules: &Rules) -> i32 {
+    0
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Rules {
+    rules: HashMap<(String, String), i32>,
+}
+
+impl Rules {
+    pub fn new() -> Self {
+        Rules {
+            rules: HashMap::new(),
+        }
+    }
+
+    pub fn add(&mut self, (from, to, value): (&str, &str, i32)) {
+        self.rules.insert((from.to_string(), to.to_string()), value);
+    }
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn parse_works() {
+        let input = "
+Alice would gain 54 happiness units by sitting next to Bob.
+Alice would lose 79 happiness units by sitting next to Carol.
+Alice would lose 2 happiness units by sitting next to David.
+Bob would gain 83 happiness units by sitting next to Alice.
+Bob would lose 7 happiness units by sitting next to Carol.
+Bob would lose 63 happiness units by sitting next to David.
+Carol would lose 62 happiness units by sitting next to Alice.
+Carol would gain 60 happiness units by sitting next to Bob.
+Carol would gain 55 happiness units by sitting next to David.
+David would gain 46 happiness units by sitting next to Alice.
+David would lose 7 happiness units by sitting next to Bob.
+David would gain 41 happiness units by sitting next to Carol.";
+
+        let mut expected_rules = Rules::new();
+        expected_rules.add(("Alice", "Bob", 54));
+        expected_rules.add(("Alice", "Carol", -79));
+        expected_rules.add(("Alice", "David", -2));
+        expected_rules.add(("Bob", "Alice", 83));
+        expected_rules.add(("Bob", "Carol", -7));
+        expected_rules.add(("Bob", "David", -63));
+        expected_rules.add(("Carol", "Alice", -62));
+        expected_rules.add(("Carol", "Bob", 60));
+        expected_rules.add(("Carol", "David", 55));
+        expected_rules.add(("David", "Alice", 46));
+        expected_rules.add(("David", "Bob", -7));
+        expected_rules.add(("David", "Carol", 41));
+
+        assert_eq!(parse(input), expected_rules);
+    }
+
+    #[test]
+    pub fn parse_rule_works() {
+        let input = "Alice would gain 54 happiness units by sitting next to Bob.";
+        assert_eq!(
+            parse_rule(input),
+            ("Alice".to_string(), "Bob".to_string(), 54)
+        );
+    }
+}
