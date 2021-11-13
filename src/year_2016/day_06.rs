2b812598f3325481d3050287e3462fb8712ee915
Manuel Krebs <me@leun4m.de>
2021-11-14 00:24:32 +0100

    2016 / Day 06 (2)

--- src/year_2016/day_06.rs
+++ src/year_2016/day_06.rs
@@ -2,29 +2,21 @@ use itertools::Itertools;
 use std::collections::HashMap;
 
 pub fn main(input: &str) {
-    let words = parse(input);
-    println!("Part 1: {}", part_one(&words));
+    let matrix = CharMatrix::build(&parse(input));
+    println!("Part 1: {}", part_one(&matrix));
+    println!("Part 2: {}", part_two(&matrix));
 }
 
 fn parse(input: &str) -> Vec<&str> {
-    input
-        .lines()
-        .filter(|x| !x.is_empty())
-        .collect()
+    input.lines().filter(|x| !x.is_empty()).collect()
 }
 
-fn part_one(words: &[&str]) -> String {
-    if words.is_empty() {
-        return String::new();
-    }
-    let first = words[0];
-    let mut matrix = CharMatrix::new(first.len());
+fn part_one(matrix: &CharMatrix) -> String {
+    matrix.most_common()
+}
 
-    for word in words {
-        matrix.add(word);
-    }
-
-    matrix.calc_best()
+fn part_two(matrix: &CharMatrix) -> String {
+    matrix.least_common()
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -34,7 +26,30 @@ struct CharMatrix {
 }
 
 impl CharMatrix {
-    pub fn new(size: usize) -> Self {
+    pub fn build(words: &[&str]) -> Self {
+        let first = words[0];
+        let mut matrix = CharMatrix::new(first.len());
+        for word in words {
+            matrix.add(word);
+        }
+        matrix
+    }
+
+    pub fn least_common(&self) -> String {
+        self.field
+            .iter()
+            .map(|x| Self::calc_least_common(x))
+            .collect()
+    }
+
+    pub fn most_common(&self) -> String {
+        self.field
+            .iter()
+            .map(|x| Self::calc_most_common(x))
+            .collect()
+    }
+
+    fn new(size: usize) -> Self {
         let mut field = Vec::new();
         for _ in 0..size {
             field.push(HashMap::new());
@@ -43,9 +58,9 @@ impl CharMatrix {
         Self { field, size }
     }
 
-    pub fn add(&mut self, word: &str) {
+    fn add(&mut self, word: &str) {
         if word.len() != self.size {
-            panic!("Word has different length")
+            panic!("Word has different length");
         }
 
         for (i, c) in word.chars().enumerate() {
@@ -53,31 +68,48 @@ impl CharMatrix {
         }
     }
 
-    pub fn calc_best(&self) -> String {
-        self.field.iter().map(|x| calc_most(x)).collect()
+    fn calc_least_common(chars: &HashMap<char, usize>) -> char {
+        chars
+            .iter()
+            .sorted_by_key(|(_, value)| *value)
+            .map(|(key, _)| *key)
+            .next()
+            .unwrap()
     }
-}
 
-fn calc_most(chars: &HashMap<char, usize>) -> char {
-    chars
-        .iter()
-        .sorted_by_key(|(_, value)| *value)
-        .map(|(key, _)| *key)
-        .last()
-        .unwrap()
+    fn calc_most_common(chars: &HashMap<char, usize>) -> char {
+        chars
+            .iter()
+            .sorted_by_key(|(_, value)| *value)
+            .map(|(key, _)| *key)
+            .last()
+            .unwrap()
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
     #[test]
     fn part_one_works() {
         assert_eq!(
-            part_one(&vec![
+            part_one(&CharMatrix::build(&vec![
                 "eedadn", "drvtee", "eandsr", "raavrd", "atevrs", "tsrnev", "sdttsa", "rasrtv",
                 "nssdts", "ntnada", "svetve", "tesnvt", "vntsnd", "vrdear", "dvrsen", "enarar"
-            ]),
+            ])),
             "easter"
+        );
+    }
+
+    #[test]
+    fn part_two_works() {
+        assert_eq!(
+            part_two(&CharMatrix::build(&vec![
+                "eedadn", "drvtee", "eandsr", "raavrd", "atevrs", "tsrnev", "sdttsa", "rasrtv",
+                "nssdts", "ntnada", "svetve", "tesnvt", "vntsnd", "vrdear", "dvrsen", "enarar"
+            ])),
+            "advent"
         );
     }
 }
