a82fc19a7aa2ea0d5d41c39ea18ca075c98a14d4
Manuel Krebs <me@leun4m.de>
2021-11-20 14:52:07 +0100

    2015 / Day 12 (1)

--- src/year_2015/day_12.rs
+++ src/year_2015/day_12.rs
@@ -1,25 +1,42 @@
 use regex::Regex;
 use std::collections::HashMap;
 
-pub fn main(input: &str) {}
+pub fn main(input: &str) {
+    println!("Part 1: {}", part_one(input));
+}
+
+fn part_one(input: &str) -> i32 {
+    let root = parse(input);
+    sum_numbers(&root)
+}
+
+fn sum_numbers(root: &JSONElement) -> i32 {
+    match root {
+        JSONElement::JSONString(_) => 0,
+        JSONElement::JSONNumber(x) => *x,
+        JSONElement::JSONArray(v) => v.iter().map(|x| sum_numbers(x)).sum(),
+        JSONElement::JSONObject(m) => m.values().map(|x| sum_numbers(x)).sum(),
+    }
+}
 
 fn parse(input: &str) -> Box<JSONElement> {
+    let trimmed = input.trim();
     let regex_number: Regex = Regex::new(r"^-?\d+$").unwrap();
     let regex_string: Regex = Regex::new(r#"^"([^"]*)"$"#).unwrap();
-    if let Some(capture) = regex_number.captures(input) {
+    if let Some(capture) = regex_number.captures(trimmed) {
         let value = capture[0].parse::<i32>().unwrap();
         return Box::new(JSONElement::JSONNumber(value));
-    } else if let Some(capture) = regex_string.captures(input) {
+    } else if let Some(capture) = regex_string.captures(trimmed) {
         let value = capture[1].parse::<String>().unwrap();
         return Box::new(JSONElement::JSONString(value));
-    } else if input.starts_with("[") && input.ends_with("]") {
-        if input == "[]" {
+    } else if trimmed.starts_with("[") && trimmed.ends_with("]") {
+        if trimmed == "[]" {
             return Box::new(JSONElement::JSONArray(Vec::new()));
         } else {
-            return parse_array(&input[1..input.len() - 1]);
+            return parse_array(&trimmed[1..trimmed.len() - 1]);
         }
-    } else if input.starts_with("{") && input.ends_with("}") {
-        if input == "{}" {
+    } else if trimmed.starts_with("{") && trimmed.ends_with("}") {
+        if trimmed == "{}" {
             return Box::new(JSONElement::JSONObject(HashMap::new()));
         } else {
             return parse_object(&input[1..input.len() - 1]);
@@ -40,10 +57,10 @@ fn parse_array(inner: &str) -> Box<JSONElement> {
             word = String::new();
         } else {
             word.push(c);
-            if c == '[' {
-                count_brackets += 1;
-            } else if c == ']' {
-                count_brackets -= 1;
+            match c {
+                '[' | '{' => count_brackets += 1,
+                ']' | '}' => count_brackets -= 1,
+                _ => {}
             }
         }
     }
@@ -59,13 +76,13 @@ fn parse_object(inner: &str) -> Box<JSONElement> {
     let mut key = String::new();
     let mut word = String::new();
     let mut count_brackets = 0;
-    let mut state = State::Start_Key;
+    let mut state = State::StartKey;
 
     for c in inner.chars() {
         if count_brackets == 0 && c == '"' && state.is_key() {
             state = match state {
-                State::Start_Key => State::End_Key,
-                State::End_Key => State::Start_Value,
+                State::StartKey => State::EndKey,
+                State::EndKey => State::StartValue,
                 x => x,
             }
         } else if count_brackets == 0 && c == ':' {
@@ -74,7 +91,7 @@ fn parse_object(inner: &str) -> Box<JSONElement> {
             a.insert(key.clone(), *parse(&word));
             key = String::new();
             word = String::new();
-            state = State::Start_Key;
+            state = State::StartKey;
         } else {
             if c == '[' || c == '{' {
                 count_brackets += 1;
@@ -83,18 +100,17 @@ fn parse_object(inner: &str) -> Box<JSONElement> {
             }
 
             match state {
-                State::Start_Key => {
+                State::StartKey => {
                     if !c.is_whitespace() {
                         key.push(c)
                     }
                 }
-                State::End_Key => key.push(c),
-                State::Start_Value => {
+                State::EndKey => key.push(c),
+                State::StartValue => {
                     if !c.is_whitespace() {
                         word.push(c)
                     }
                 }
-                _ => {}
             }
         }
     }
@@ -104,16 +120,16 @@ fn parse_object(inner: &str) -> Box<JSONElement> {
 }
 
 enum State {
-    Start_Key,
-    End_Key,
-    Start_Value,
-    End_Value,
+    StartKey,
+    EndKey,
+    StartValue,
+    // EndValue,
 }
 
 impl State {
     fn is_key(&self) -> bool {
         match self {
-            State::Start_Key | State::End_Key => true,
+            State::StartKey | State::EndKey => true,
             _ => false,
         }
     }
@@ -191,5 +207,20 @@ mod test {
                 ("b".into(), JSONElement::JSONNumber(123))
             ])))
         );
+    }
+
+    #[test]
+    fn part_one_works() {
+        assert_eq!(part_one(r#"[1,2,3]"#), 6);
+        assert_eq!(part_one(r#"{"a":2,"b":4}"#), 6);
+
+        assert_eq!(part_one(r#"[[[3]]]"#), 3);
+        assert_eq!(part_one(r#"{"a":{"b":4},"c":-1}"#), 3);
+
+        assert_eq!(part_one(r#"{"a":[-1,1]}"#), 0);
+        assert_eq!(part_one(r#"[-1,{"a":1}]"#), 0);
+
+        assert_eq!(part_one(r#"[]"#), 0);
+        assert_eq!(part_one(r#"{}"#), 0);
     }
 }
