73ffaa7730f4529cc9d18b81b158be7cc133db3e
Manuel Krebs <me@leun4m.de>
2021-11-22 23:23:01 +0100

    2015 / Day 17 (Part 2)

--- src/year_2015/day_17.rs
+++ src/year_2015/day_17.rs
@@ -1,5 +1,6 @@
-use std::collections::HashSet;
 use itertools::Itertools;
+use std::cmp::Ordering;
+use std::collections::HashSet;
 
 type BucketSize = u32;
 
@@ -14,8 +15,10 @@ const GOAL: BucketSize = 150;
 pub fn main(input: &str) {
     let buckets = parse(input);
 
-    println!("Part 1: {}", part_one(&buckets));
-    println!("Part 2: {}", part_two(&buckets));
+    let p = find_fitting(&buckets, GOAL);
+
+    println!("Part 1: {}", part_one(&p));
+    println!("Part 2: {}", part_two(&p));
 }
 
 fn parse(input: &str) -> Vec<Bucket> {
@@ -23,22 +26,23 @@ fn parse(input: &str) -> Vec<Bucket> {
         .lines()
         .filter(|line| !line.is_empty())
         .map(|line| line.parse::<BucketSize>().unwrap())
-        .sorted_by(|a, b| b.cmp(&a))
+        .sorted_by(|a, b| b.cmp(a))
         .zip(0..)
         .map(|(a, b)| Bucket { id: b, litre: a })
         .collect()
 }
 
-fn part_one(buckets: &[Bucket]) -> usize {
-    find_fitting(buckets, GOAL)
+fn part_one(buckets: &HashSet<Vec<Bucket>>) -> usize {
+    buckets.len()
 }
 
-fn part_two(buckets: &[Bucket]) -> u32 {
-    0
+fn part_two(buckets: &HashSet<Vec<Bucket>>) -> usize {
+    let minimum_amount = buckets.iter().map(|x| x.len()).min().unwrap_or_default();
+    buckets.iter().filter(|x| x.len() == minimum_amount).count()
 }
 
-fn find_fitting(buckets: &[Bucket], goal: BucketSize) -> usize {
-    find_possibilities(&buckets[0..1], &buckets[1..], goal).len()
+fn find_fitting(buckets: &[Bucket], goal: BucketSize) -> HashSet<Vec<Bucket>> {
+    find_possibilities(&buckets[0..1], &buckets[1..], goal)
 }
 
 fn find_possibilities(a: &[Bucket], b: &[Bucket], goal: BucketSize) -> HashSet<Vec<Bucket>> {
@@ -50,14 +54,19 @@ fn find_possibilities(a: &[Bucket], b: &[Bucket], goal: BucketSize) -> HashSet<V
     }
 
     let x = sum_a + b[0].litre;
-    if x == goal {
-        let mut vec = Vec::from(a);
-        vec.push(b[0]);
-        result.insert(vec);
-    } else if x < goal {
-        let mut vec = Vec::from(a);
-        vec.push(b[0]);
-        result.extend(find_possibilities(&vec, &b[1..], goal));
+
+    match x.cmp(&goal) {
+        Ordering::Equal => {
+            let mut vec = Vec::from(a);
+            vec.push(b[0]);
+            result.insert(vec);
+        }
+        Ordering::Less => {
+            let mut vec = Vec::from(a);
+            vec.push(b[0]);
+            result.extend(find_possibilities(&vec, &b[1..], goal));
+        }
+        Ordering::Greater => {}
     }
 
     result.extend(find_possibilities(a, &b[1..], goal));
@@ -85,7 +94,16 @@ mod test {
         );
         assert_eq!(
             find_fitting(&a, 25),
-            4
+            HashSet::from([
+                vec![Bucket { id: 0, litre: 20 }, Bucket { id: 3, litre: 5 }],
+                vec![Bucket { id: 0, litre: 20 }, Bucket { id: 4, litre: 5 }],
+                vec![Bucket { id: 1, litre: 15 }, Bucket { id: 2, litre: 10 }],
+                vec![
+                    Bucket { id: 1, litre: 15 },
+                    Bucket { id: 3, litre: 5 },
+                    Bucket { id: 4, litre: 5 }
+                ],
+            ])
         );
     }
 }
