93747ac941bd120bd0a6f3fbd09774bdfbb54436
Manuel Krebs <me@leun4m.de>
2021-11-22 23:28:26 +0100

    2015 / Day 17 (Refactor)

--- src/year_2015/day_17.rs
+++ src/year_2015/day_17.rs
@@ -7,18 +7,17 @@ type BucketSize = u32;
 #[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
 struct Bucket {
     id: usize,
-    litre: BucketSize,
+    size: BucketSize,
 }
 
 const GOAL: BucketSize = 150;
 
 pub fn main(input: &str) {
     let buckets = parse(input);
+    let arrangements = find_arrangements(&buckets, GOAL);
 
-    let p = find_fitting(&buckets, GOAL);
-
-    println!("Part 1: {}", part_one(&p));
-    println!("Part 2: {}", part_two(&p));
+    println!("Part 1: {}", part_one(&arrangements));
+    println!("Part 2: {}", part_two(&arrangements));
 }
 
 fn parse(input: &str) -> Vec<Bucket> {
@@ -28,34 +27,40 @@ fn parse(input: &str) -> Vec<Bucket> {
         .map(|line| line.parse::<BucketSize>().unwrap())
         .sorted_by(|a, b| b.cmp(a))
         .zip(0..)
-        .map(|(a, b)| Bucket { id: b, litre: a })
+        .map(|(a, b)| Bucket { id: b, size: a })
         .collect()
 }
 
-fn part_one(buckets: &HashSet<Vec<Bucket>>) -> usize {
-    buckets.len()
+fn part_one(arrangements: &HashSet<Vec<Bucket>>) -> usize {
+    arrangements.len()
 }
 
-fn part_two(buckets: &HashSet<Vec<Bucket>>) -> usize {
-    let minimum_amount = buckets.iter().map(|x| x.len()).min().unwrap_or_default();
-    buckets.iter().filter(|x| x.len() == minimum_amount).count()
+fn part_two(arrangements: &HashSet<Vec<Bucket>>) -> usize {
+    let minimum_amount = arrangements
+        .iter()
+        .map(|x| x.len())
+        .min()
+        .unwrap_or_default();
+    arrangements
+        .iter()
+        .filter(|x| x.len() == minimum_amount)
+        .count()
 }
 
-fn find_fitting(buckets: &[Bucket], goal: BucketSize) -> HashSet<Vec<Bucket>> {
-    find_possibilities(&buckets[0..1], &buckets[1..], goal)
+fn find_arrangements(buckets: &[Bucket], goal: BucketSize) -> HashSet<Vec<Bucket>> {
+    find_sub_arrangements(&buckets[0..1], &buckets[1..], goal)
 }
 
-fn find_possibilities(a: &[Bucket], b: &[Bucket], goal: BucketSize) -> HashSet<Vec<Bucket>> {
+fn find_sub_arrangements(a: &[Bucket], b: &[Bucket], goal: BucketSize) -> HashSet<Vec<Bucket>> {
     let mut result = HashSet::new();
-
-    let sum_a = a.iter().map(|x| x.litre).sum::<BucketSize>();
-    if b.is_empty() || sum_a >= goal {
+    
+    let a_sum = a.iter().map(|x| x.size).sum::<BucketSize>();
+    
+    if b.is_empty() || a_sum >= goal {
         return result;
     }
 
-    let x = sum_a + b[0].litre;
-
-    match x.cmp(&goal) {
+    match (a_sum + b[0].size).cmp(&goal) {
         Ordering::Equal => {
             let mut vec = Vec::from(a);
             vec.push(b[0]);
@@ -64,16 +69,16 @@ fn find_possibilities(a: &[Bucket], b: &[Bucket], goal: BucketSize) -> HashSet<V
         Ordering::Less => {
             let mut vec = Vec::from(a);
             vec.push(b[0]);
-            result.extend(find_possibilities(&vec, &b[1..], goal));
+            result.extend(find_sub_arrangements(&vec, &b[1..], goal));
         }
         Ordering::Greater => {}
     }
 
-    result.extend(find_possibilities(a, &b[1..], goal));
+    result.extend(find_sub_arrangements(a, &b[1..], goal));
 
     let mut vec = Vec::from(&a[1..]);
     vec.push(b[0]);
-    result.extend(find_possibilities(&vec, &b[1..], goal));
+    result.extend(find_sub_arrangements(&vec, &b[1..], goal));
 
     result
 }
@@ -93,15 +98,15 @@ mod test {
 5",
         );
         assert_eq!(
-            find_fitting(&a, 25),
+            find_arrangements(&a, 25),
             HashSet::from([
-                vec![Bucket { id: 0, litre: 20 }, Bucket { id: 3, litre: 5 }],
-                vec![Bucket { id: 0, litre: 20 }, Bucket { id: 4, litre: 5 }],
-                vec![Bucket { id: 1, litre: 15 }, Bucket { id: 2, litre: 10 }],
+                vec![Bucket { id: 0, size: 20 }, Bucket { id: 3, size: 5 }],
+                vec![Bucket { id: 0, size: 20 }, Bucket { id: 4, size: 5 }],
+                vec![Bucket { id: 1, size: 15 }, Bucket { id: 2, size: 10 }],
                 vec![
-                    Bucket { id: 1, litre: 15 },
-                    Bucket { id: 3, litre: 5 },
-                    Bucket { id: 4, litre: 5 }
+                    Bucket { id: 1, size: 15 },
+                    Bucket { id: 3, size: 5 },
+                    Bucket { id: 4, size: 5 }
                 ],
             ])
         );
