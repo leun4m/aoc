0624a26e47479871d4820ef6c586ae22ad3933b4
Manuel Krebs <me@leun4m.de>
2021-11-23 23:11:25 +0100

    2017 / Day 01 (Part 2)

--- src/year_2017/day_01.rs
+++ src/year_2017/day_01.rs
@@ -1,7 +1,7 @@
 pub fn main(input: &str) {
     let trimmed = input.trim();
     println!("{}", part_one(trimmed));
-    // println!("{}", part_two(input));
+    println!("{}", part_two(trimmed));
 }
 
 fn part_one(input: &str) -> u32 {
@@ -22,6 +22,27 @@ fn part_one(input: &str) -> u32 {
     sum
 }
 
+fn part_two(input: &str) -> u32 {
+    if input.len() < 2 {
+        return 0;
+    }
+
+    let mut sum = 0;
+    let steps = input.len() / 2;
+    let chars = input.chars().collect::<Vec<char>>();
+    
+    for i in 0..input.len() {
+        let a = chars[i % input.len()];
+        let b = chars[(i + steps) % input.len()];
+
+        if a == b {
+            sum += a.to_digit(10).unwrap();
+        }
+    }
+
+    sum
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -33,5 +54,14 @@ mod test {
         assert_eq!(part_one("1234"), 0);
         assert_eq!(part_one("91212129"), 9);
         assert_eq!(part_one("91212199"), 18);
+    }
+
+    #[test]
+    pub fn part_two_works() {
+        assert_eq!(part_two("1212"), 6);
+        assert_eq!(part_two("1221"), 0);
+        assert_eq!(part_two("123425"), 4);
+        assert_eq!(part_two("123123"), 12);
+        assert_eq!(part_two("12131415"), 4);
     }
 }
