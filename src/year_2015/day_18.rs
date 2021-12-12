77ee82ce6ab68fb65eee57fd951ac4bd36251291
Manuel Krebs <me@leun4m.de>
2021-12-12 20:56:52 +0100

    2015 / Day 18 (Refactor)

--- src/year_2015/day_18.rs
+++ src/year_2015/day_18.rs
@@ -25,29 +25,29 @@ fn parse(input: &str) -> LightGrid {
 }
 
 fn part_one(start: &[LightRow], steps: u32) -> usize {
-    let switch_on = |x: isize, y: isize, lights: &[LightRow]| {
-        let neighbours = count_neighbours(x as isize, y as isize, lights);
-        neighbours == 3 || (neighbours == 2 && lights[x as usize][y as usize])
+    let switch_on = |x: usize, y: usize, lights: &[LightRow]| {
+        let neighbours = count_neighbours(lights, x as isize, y as isize);
+        neighbours == 3 || (neighbours == 2 && lights[x][y])
     };
 
-    (0..steps)
-        .collect::<Vec<_>>()
-        .iter()
-        .fold(start.to_owned(), |acc, _| next_step(&acc, switch_on))
-        .into_iter()
-        .flatten()
-        .filter(|&x| x)
-        .count()
+    iterate(start, steps, switch_on)
 }
 
 fn part_two(start: &[LightRow], steps: u32) -> usize {
-    let switch_on = |x: isize, y: isize, lights: &[LightRow]| {
-        let neighbours = count_neighbours(x as isize, y as isize, lights);
+    let switch_on = |x: usize, y: usize, lights: &[LightRow]| {
+        let neighbours = count_neighbours(lights, x as isize, y as isize);
         neighbours == 3
-            || (neighbours == 2 && lights[x as usize][y as usize])
-            || is_corner(x as usize, y as usize, lights)
+            || (neighbours == 2 && lights[x][y])
+            || is_corner(x, y, lights)
     };
 
+    iterate(start, steps, switch_on)
+}
+
+fn iterate<F>(start: &[LightRow], steps: u32, switch_on: F) -> usize
+where
+    F: Copy + Fn(usize, usize, &[LightRow]) -> bool,
+{
     (0..steps)
         .collect::<Vec<_>>()
         .iter()
@@ -60,7 +60,7 @@ fn part_two(start: &[LightRow], steps: u32) -> usize {
 
 fn next_step<F>(lights: &[LightRow], switch_on: F) -> LightGrid
 where
-    F: Fn(isize, isize, &[LightRow]) -> bool,
+    F: Copy + Fn(usize, usize, &[LightRow]) -> bool,
 {
     lights
         .iter()
@@ -68,26 +68,26 @@ where
         .map(|(x, row)| {
             row.iter()
                 .enumerate()
-                .map(|(y, _)| switch_on(x as isize, y as isize, lights))
+                .map(|(y, _)| switch_on(x, y, lights))
                 .collect()
         })
         .collect()
 }
 
-fn count_neighbours(x: isize, y: isize, lights: &[LightRow]) -> u32 {
+fn count_neighbours(lights: &[LightRow], x: isize, y: isize) -> u32 {
     let mut result = 0;
-    result += value(x - 1, y - 1, lights);
-    result += value(x - 1, y, lights);
-    result += value(x - 1, y + 1, lights);
-    result += value(x, y - 1, lights);
-    result += value(x, y + 1, lights);
-    result += value(x + 1, y - 1, lights);
-    result += value(x + 1, y, lights);
-    result += value(x + 1, y + 1, lights);
+    result += numeric(lights, x - 1, y - 1);
+    result += numeric(lights, x - 1, y);
+    result += numeric(lights, x - 1, y + 1);
+    result += numeric(lights, x, y - 1);
+    result += numeric(lights, x, y + 1);
+    result += numeric(lights, x + 1, y - 1);
+    result += numeric(lights, x + 1, y);
+    result += numeric(lights, x + 1, y + 1);
     result
 }
 
-fn value(x: isize, y: isize, lights: &[LightRow]) -> u32 {
+fn numeric(lights: &[LightRow], x: isize, y: isize) -> u32 {
     let max_x = lights.len();
     let max_y = lights[0].len();
 
