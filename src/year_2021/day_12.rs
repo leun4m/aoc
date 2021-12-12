e1c3e4ccaf6763509e598f4eb6cc7bc66826c329
Manuel Krebs <me@leun4m.de>
2021-12-12 12:43:29 +0100

    2021 / Day 12

---
+++ src/year_2021/day_12.rs
@@ -0,0 +1,126 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+
+pub fn solve(input: &str) {
+    let graph = parse(input);
+    println!("Part 1: {}", part_one(&graph));
+    println!("Part 2: {}", part_two(&graph));
+}
+
+type Cave<'a> = &'a str;
+type Graph<'a> = HashMap<Cave<'a>, Vec<Cave<'a>>>;
+
+const START: &str = "start";
+const END: &str = "end";
+
+fn parse(input: &str) -> Graph {
+    let mut graph: Graph = HashMap::new();
+
+    for (a, b) in input
+        .lines()
+        .filter(|line| !line.trim().is_empty())
+        .map(|line| parse_pair(line))
+    {
+        (*graph.entry(a).or_default()).push(b);
+        (*graph.entry(b).or_default()).push(a);
+    }
+
+    graph
+}
+
+fn parse_pair(input: &str) -> (Cave, Cave) {
+    let words: Vec<&str> = input.trim().split('-').collect();
+    (words[0], words[1])
+}
+
+fn part_one(graph: &Graph) -> usize {
+    endings(graph, &[], START, can_been_visited_again_one).len()
+}
+
+fn part_two(graph: &Graph) -> usize {
+    endings(graph, &[], START, can_been_visited_again_two).len()
+}
+
+fn can_been_visited_again_one(cave: Cave, visited: &[Cave]) -> bool {
+    is_big_cave(cave) || !visited.contains(&cave)
+}
+
+fn can_been_visited_again_two(cave: Cave, visited: &[Cave]) -> bool {
+    if is_big_cave(cave) || !visited.contains(&cave) {
+        return true;
+    }
+
+    let small_caves = visited.iter().filter(|x| !is_big_cave(x)).count();
+    let unique_small_caves = visited.iter().filter(|x| !is_big_cave(x)).unique().count();
+
+    cave != START && cave != END && unique_small_caves == small_caves
+}
+
+fn endings<'a, F>(
+    graph: &'a Graph,
+    visited: &[Cave],
+    start: Cave,
+    can_be_visited: F,
+) -> Vec<Vec<Cave<'a>>>
+where
+    F: Copy + Fn(&str, &[Cave]) -> bool,
+{
+    if start == END {
+        vec![vec![END]]
+    } else {
+        let mut new_visited = visited.to_owned();
+        new_visited.push(start);
+
+        let mut result = Vec::new();
+        for next in graph.get(start).expect("Node not in graph") {
+            if can_be_visited(next, &new_visited) {
+                for ends in endings(graph, &new_visited, next, can_be_visited) {
+                    result.push(ends);
+                }
+            }
+        }
+        result
+    }
+}
+
+fn is_big_cave(cave: Cave) -> bool {
+    cave == cave.to_uppercase()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "start-A
+    start-b
+    A-c
+    A-b
+    b-d
+    A-end
+    b-end";
+
+    #[test]
+    fn parse_works() {
+        assert_eq!(
+            parse(
+                "start-A
+            start-b"
+            ),
+            HashMap::from([
+                ("start", vec!["A", "b"]),
+                ("A", vec!["start"]),
+                ("b", vec!["start"])
+            ])
+        );
+    }
+
+    #[test]
+    fn part_one_works() {
+        assert_eq!(part_one(&parse(INPUT)), 10);
+    }
+
+    #[test]
+    fn part_two_works() {
+        assert_eq!(part_two(&parse(INPUT)), 36)
+    }
+}
--- src/year_2021/mod.rs
+++ src/year_2021/mod.rs
@@ -10,6 +10,7 @@ mod day_07;
 mod day_09;
 mod day_10;
 mod day_11;
+mod day_12;
 
 pub fn solve_day(day: u8, input: &str) {
     match day {
@@ -23,6 +24,7 @@ pub fn solve_day(day: u8, input: &str) {
         9 => day_09::solve(input),
         10 => day_10::solve(input),
         11 => day_11::solve(input),
+        12 => day_12::solve(input),
         _ => println!("{}", INVALID_DAY),
     };
 }
