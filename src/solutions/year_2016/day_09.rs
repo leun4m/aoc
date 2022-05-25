d1bb8c96628553e82fac8be3693c71e85d1be26b
Manuel Krebs <me@leun4m.de>
2022-05-25 03:14:45 +0200

    2016 / Day 09 (Refactor)

--- src/solutions/year_2016/day_09.rs
+++ src/solutions/year_2016/day_09.rs
@@ -1,92 +1,93 @@
 pub fn solve(input: &str) {
     let sanatized_input = sanatize(input);
-    println!("Part 1: {}", decompress_v1(&sanatized_input).len());
-    println!("Part 2: {}", decompress_v2_len(&sanatized_input));
+    println!("Part 1: {}", part_one(&sanatized_input));
+    println!("Part 2: {}", part_two(&sanatized_input));
+}
+
+fn calc_decompressed_len<F>(input: &str, parser: F) -> usize
+where
+    F: Fn(char, &mut std::str::Chars) -> Token,
+{
+    parse(input, parser).iter().map(Token::size).sum::<usize>()
+}
+
+fn part_one(input: &str) -> usize {
+    calc_decompressed_len(input, parse_token_v1)
+}
+
+fn part_two(input: &str) -> usize {
+    calc_decompressed_len(input, parse_token_v2)
 }
 
 fn sanatize(input: &str) -> String {
     input.replace(|c: char| c.is_ascii_whitespace(), "")
 }
 
-fn decompress_v1(input: &str) -> String {
-    let mut result = String::new();
-
-    let mut chars = input.chars();
-    while let Some(x) = chars.next() {
-        match x {
-            '(' => {
-                let count = read_next_num(&mut chars);
-                let times = read_next_num(&mut chars);
-
-                result.push_str(&extract_chars(&mut chars, count).repeat(times));
-            }
-            c => result.push(c),
-        };
-    }
-
-    result
-}
-
-fn decompress_v2_len(input: &str) -> usize {
-    parse(input).iter().map(|x| x.size()).sum()
-}
-
 #[derive(Debug, PartialEq, Eq)]
 enum Token {
-    Repeat(Vec<Token>, usize),
+    RepeatV1(String, usize),
+    RepeatV2(Vec<Token>, usize),
     Char(char),
 }
 
 impl Token {
     fn size(&self) -> usize {
         match self {
-            Token::Repeat(vec, size) => vec.iter().map(|x| x.size()).sum::<usize>() * size,
+            Token::RepeatV1(word, size) => word.len() * size,
+            Token::RepeatV2(vec, size) => vec.iter().map(|x| x.size()).sum::<usize>() * size,
             Token::Char(_) => 1,
         }
     }
 }
 
-fn parse(input: &str) -> Vec<Token> {
+fn parse<F>(input: &str, parse_token: F) -> Vec<Token>
+where
+    F: Fn(char, &mut std::str::Chars) -> Token,
+{
     let mut result = Vec::new();
     let mut chars = input.chars();
 
     while let Some(x) = chars.next() {
-        result.push(match x {
-            '(' => {
-                let count = read_next_num(&mut chars);
-                let times = read_next_num(&mut chars);
-
-                let next = extract_chars(&mut chars, count);
-                Token::Repeat(parse(&next), times)
-            }
-            c => Token::Char(c),
-        });
+        result.push(parse_token(x, &mut chars));
     }
 
     result
 }
 
-fn extract_chars(chars: &mut std::str::Chars, count: usize) -> String {
-    let mut result = String::new();
+fn parse_token_v1(x: char, chars: &mut std::str::Chars) -> Token {
+    match x {
+        '(' => {
+            let count = read_next_num(chars);
+            let times = read_next_num(chars);
+            let next = chars.take(count).collect::<String>();
 
-    for c in chars.take(count) {
-        result.push(c);
+            Token::RepeatV1(next, times)
+        }
+        c => Token::Char(c),
     }
+}
 
-    result
+fn parse_token_v2(x: char, chars: &mut std::str::Chars) -> Token {
+    match x {
+        '(' => {
+            let count = read_next_num(chars);
+            let times = read_next_num(chars);
+            let next = chars.take(count).collect::<String>();
+
+            Token::RepeatV2(parse(&next, parse_token_v2), times)
+        }
+        c => Token::Char(c),
+    }
 }
 
 fn read_next_num(chars: &mut std::str::Chars) -> usize {
     let mut num = String::new();
-    let mut found_end = true;
 
-    while found_end {
-        if let Some(x) = chars.next() {
-            if x.is_digit(10) {
-                num.push(x);
-            } else {
-                found_end = false;
-            }
+    while let Some(x) = chars.next() {
+        if x.is_digit(10) {
+            num.push(x);
+        } else {
+            break;
         }
     }
 
@@ -99,15 +100,15 @@ mod tests {
 
     #[test]
     fn decompress_works() {
-        assert_eq!(&decompress_v1("ADVENT"), "ADVENT");
-        assert_eq!(&decompress_v1("A(1x5)BC"), "ABBBBBC");
-        assert_eq!(&decompress_v1("(3x3)XYZ"), "XYZXYZXYZ");
+        assert_eq!(part_one("ADVENT"), "ADVENT".len());
+        assert_eq!(part_one("A(1x5)BC"), "ABBBBBC".len());
+        assert_eq!(part_one("(3x3)XYZ"), "XYZXYZXYZ".len());
     }
-    
+
     #[test]
     fn decompress_v2_works() {
-        assert_eq!(decompress_v2_len("(3x3)XYZ"), 9);
-        assert_eq!(decompress_v2_len("X(8x2)(3x3)ABCY"), 20);
-        assert_eq!(decompress_v2_len("(27x12)(20x12)(13x14)(7x10)(1x12)A"), 241920);
+        assert_eq!(part_two("(3x3)XYZ"), 9);
+        assert_eq!(part_two("X(8x2)(3x3)ABCY"), 20);
+        assert_eq!(part_two("(27x12)(20x12)(13x14)(7x10)(1x12)A"), 241920);
     }
 }
