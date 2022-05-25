34d3caa539d5dad676fdbfbbb8519f73bc7a59ce
Manuel Krebs <me@leun4m.de>
2022-05-25 03:01:52 +0200

    2016 / Day 09 (Part 2)

--- src/solutions/year_2016/day_09.rs
+++ src/solutions/year_2016/day_09.rs
@@ -1,17 +1,17 @@
 pub fn solve(input: &str) {
-    let decompressed = decompress(input);
-    println!("Part 1: {}", decompressed.len())
+    let sanatized_input = sanatize(input);
+    println!("Part 1: {}", decompress_v1(&sanatized_input).len());
+    println!("Part 2: {}", decompress_v2_len(&sanatized_input));
 }
 
 fn sanatize(input: &str) -> String {
     input.replace(|c: char| c.is_ascii_whitespace(), "")
 }
 
-fn decompress(input: &str) -> String {
+fn decompress_v1(input: &str) -> String {
     let mut result = String::new();
 
-    let s = sanatize(input);
-    let mut chars = s.chars();
+    let mut chars = input.chars();
     while let Some(x) = chars.next() {
         match x {
             '(' => {
@@ -27,9 +27,48 @@ fn decompress(input: &str) -> String {
     result
 }
 
+fn decompress_v2_len(input: &str) -> usize {
+    parse(input).iter().map(|x| x.size()).sum()
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Token {
+    Repeat(Vec<Token>, usize),
+    Char(char),
+}
+
+impl Token {
+    fn size(&self) -> usize {
+        match self {
+            Token::Repeat(vec, size) => vec.iter().map(|x| x.size()).sum::<usize>() * size,
+            Token::Char(_) => 1,
+        }
+    }
+}
+
+fn parse(input: &str) -> Vec<Token> {
+    let mut result = Vec::new();
+    let mut chars = input.chars();
+
+    while let Some(x) = chars.next() {
+        result.push(match x {
+            '(' => {
+                let count = read_next_num(&mut chars);
+                let times = read_next_num(&mut chars);
+
+                let next = extract_chars(&mut chars, count);
+                Token::Repeat(parse(&next), times)
+            }
+            c => Token::Char(c),
+        });
+    }
+
+    result
+}
+
 fn extract_chars(chars: &mut std::str::Chars, count: usize) -> String {
     let mut result = String::new();
-    
+
     for c in chars.take(count) {
         result.push(c);
     }
@@ -50,7 +89,7 @@ fn read_next_num(chars: &mut std::str::Chars) -> usize {
             }
         }
     }
-    
+
     num.parse().unwrap()
 }
 
@@ -60,8 +99,15 @@ mod tests {
 
     #[test]
     fn decompress_works() {
-        assert_eq!(&decompress("ADVENT"), "ADVENT");
-        assert_eq!(&decompress("A(1x5)BC"), "ABBBBBC");
-        assert_eq!(&decompress("(3x3)XYZ"), "XYZXYZXYZ");
+        assert_eq!(&decompress_v1("ADVENT"), "ADVENT");
+        assert_eq!(&decompress_v1("A(1x5)BC"), "ABBBBBC");
+        assert_eq!(&decompress_v1("(3x3)XYZ"), "XYZXYZXYZ");
+    }
+    
+    #[test]
+    fn decompress_v2_works() {
+        assert_eq!(decompress_v2_len("(3x3)XYZ"), 9);
+        assert_eq!(decompress_v2_len("X(8x2)(3x3)ABCY"), 20);
+        assert_eq!(decompress_v2_len("(27x12)(20x12)(13x14)(7x10)(1x12)A"), 241920);
     }
 }
