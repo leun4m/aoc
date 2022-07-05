62779e1c05a86351ca7c8dd5aab388a4da8038c7
Manuel Krebs <me@leun4m.de>
2022-07-05 03:11:41 +0200

    2017 / Day 03 (Refactor)

--- src/solutions/year_2017/day_03.rs
+++ src/solutions/year_2017/day_03.rs
@@ -8,19 +8,20 @@ pub fn solve(input: &str) {
 
 fn part_one(aim: u32) -> i32 {
     let mut point = Point::default();
-    let mut direction = Direction::Right;
+    let mut direction = Direction::default();
     let mut boundaries = Boundaries::default();
 
     for _ in 1..aim {
-        point = next_step(&point, &mut direction, &mut boundaries);
+        point = point.move_this(&direction);
+        direction = change_dir(&point, &direction, &mut boundaries);
     }
 
-    manhatten_distance(point)
+    manhatten_distance(&point)
 }
 
 fn part_two(aim: u32) -> u32 {
     let mut point = Point::default();
-    let mut direction = Direction::Right;
+    let mut direction = Direction::default();
     let mut boundaries = Boundaries::default();
 
     let mut cache: HashMap<Point, u32> = HashMap::new();
@@ -28,7 +29,10 @@ fn part_two(aim: u32) -> u32 {
 
     while sum <= aim {
         cache.insert(point, sum);
-        point = next_step(&point, &mut direction, &mut boundaries);
+
+        point = point.move_this(&direction);
+        direction = change_dir(&point, &direction, &mut boundaries);
+
         sum = sum_neighbours(point, &cache);
     }
 
@@ -36,73 +40,125 @@ fn part_two(aim: u32) -> u32 {
 }
 
 fn sum_neighbours(point: Point, cache: &HashMap<Point, u32>) -> u32 {
-    cache.get(&point.left()).unwrap_or(&0)
-        + cache.get(&point.top_left()).unwrap_or(&0)
-        + cache.get(&point.top()).unwrap_or(&0)
-        + cache.get(&point.top_right()).unwrap_or(&0)
-        + cache.get(&point.right()).unwrap_or(&0)
-        + cache.get(&point.bottom_right()).unwrap_or(&0)
-        + cache.get(&point.bottom()).unwrap_or(&0)
-        + cache.get(&point.bottom_left()).unwrap_or(&0)
+    [
+        point.left(),
+        point.right(),
+        point.top(),
+        point.top().left(),
+        point.top().right(),
+        point.bottom(),
+        point.bottom().left(),
+        point.bottom().right(),
+    ]
+    .iter()
+    .map(|x| cache.get(x).unwrap_or(&0))
+    .sum()
 }
 
-fn next_step(point: &Point, direction: &mut Direction, boundaries: &mut Boundaries) -> Point {
+fn change_dir(point: &Point, direction: &Direction, boundaries: &mut Boundaries) -> Direction {
     match *direction {
         Direction::Left => {
-            if point.x == boundaries.min.x {
-                boundaries.min = boundaries.min.left();
-                *direction = Direction::Down;
+            if boundaries.has_reached_left(point) {
+                boundaries.set_min(boundaries.min().left());
+                Direction::Down
+            } else {
+                *direction
             }
-            point.left()
         }
         Direction::Right => {
-            if point.x == boundaries.max.x {
-                boundaries.max = boundaries.max.right();
-                *direction = Direction::Up;
+            if boundaries.has_reached_right(point) {
+                boundaries.set_max(boundaries.max().right());
+                Direction::Up
+            } else {
+                *direction
             }
-            point.right()
         }
         Direction::Down => {
-            if point.y == boundaries.max.y {
-                boundaries.max = boundaries.max.bottom();
-                *direction = Direction::Right;
+            if boundaries.has_reached_bottom(point) {
+                boundaries.set_max(boundaries.max().bottom());
+                Direction::Right
+            } else {
+                *direction
             }
-            point.bottom()
         }
         Direction::Up => {
-            if point.y == boundaries.min.y {
-                boundaries.min = boundaries.min.top();
-                *direction = Direction::Left;
+            if boundaries.has_reached_top(point) {
+                boundaries.set_min(boundaries.min().top());
+                Direction::Left
+            } else {
+                *direction
             }
-            point.top()
         }
     }
 }
 
-fn manhatten_distance(point: Point) -> i32 {
+fn manhatten_distance(point: &Point) -> i32 {
     point.x.abs() + point.y.abs()
 }
 
+#[derive(Clone, Copy)]
 enum Direction {
-    Left,
     Right,
     Up,
     Down,
+    Left,
+}
+
+impl Default for Direction {
+    fn default() -> Self {
+        Self::Right
+    }
 }
 
 #[derive(Debug, Default)]
 struct Boundaries {
-    pub min: Point,
-    pub max: Point,
+    min: Point,
+    max: Point,
+}
+
+impl Boundaries {
+    fn min(&self) -> &Point {
+        &self.min
+    }
+    fn set_min(&mut self, min: Point) {
+        self.min = min;
+    }
+
+    fn max(&self) -> &Point {
+        &self.max
+    }
+    fn set_max(&mut self, max: Point) {
+        self.max = max;
+    }
+
+    fn has_reached_left(&self, point: &Point) -> bool {
+        point.x() < self.min().x()
+    }
+    fn has_reached_right(&self, point: &Point) -> bool {
+        point.x() > self.max().x()
+    }
+    fn has_reached_top(&self, point: &Point) -> bool {
+        point.y() < self.min().y()
+    }
+    fn has_reached_bottom(&self, point: &Point) -> bool {
+        point.y() > self.max().y()
+    }
 }
 
 #[derive(Debug, Default, Hash, Eq, PartialEq, PartialOrd, Ord, Clone, Copy)]
 struct Point {
-    pub x: i32,
-    pub y: i32,
+    x: i32,
+    y: i32,
 }
 
 impl Point {
+    fn x(&self) -> i32 {
+        self.x
+    }
+    fn y(&self) -> i32 {
+        self.y
+    }
+
     fn left(&self) -> Self {
         Self {
             x: self.x - 1,
@@ -121,34 +177,19 @@ impl Point {
             y: self.y - 1,
         }
     }
-    fn top_left(&self) -> Self {
-        Self {
-            x: self.x - 1,
-            y: self.y - 1,
-        }
-    }
-    fn top_right(&self) -> Self {
-        Self {
-            x: self.x + 1,
-            y: self.y - 1,
-        }
-    }
     fn bottom(&self) -> Self {
         Self {
             x: self.x,
             y: self.y + 1,
         }
     }
-    fn bottom_left(&self) -> Self {
-        Self {
-            x: self.x - 1,
-            y: self.y + 1,
-        }
-    }
-    fn bottom_right(&self) -> Self {
-        Self {
-            x: self.x + 1,
-            y: self.y + 1,
+
+    fn move_this(&self, direction: &Direction) -> Point {
+        match direction {
+            Direction::Left => self.left(),
+            Direction::Right => self.right(),
+            Direction::Down => self.bottom(),
+            Direction::Up => self.top(),
         }
     }
 }
