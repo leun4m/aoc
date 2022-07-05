3afe419a3abbd82341a76ff775a071e835ea2e38
Manuel Krebs <me@leun4m.de>
2022-07-05 02:26:02 +0200

    2017 / Day 03 (Part 2)

--- src/solutions/year_2017/day_03.rs
+++ src/solutions/year_2017/day_03.rs
@@ -1,49 +1,80 @@
+use std::collections::HashMap;
+
 pub fn solve(input: &str) {
     let aim = input.parse().unwrap();
     println!("Part 1: {}", part_one(aim));
+    println!("Part 2: {}", part_two(aim));
 }
 
-fn part_one(start: u32) -> i32 {
+fn part_one(aim: u32) -> i32 {
     let mut point = Point::default();
     let mut direction = Direction::Right;
     let mut boundaries = Boundaries::default();
 
-    for _ in 1..start {
-        next_step(&mut point, &mut direction, &mut boundaries);
+    for _ in 1..aim {
+        point = next_step(&point, &mut direction, &mut boundaries);
     }
 
     manhatten_distance(point)
 }
 
-fn next_step(point: &mut Point, direction: &mut Direction, boundaries: &mut Boundaries) {
+fn part_two(aim: u32) -> u32 {
+    let mut point = Point::default();
+    let mut direction = Direction::Right;
+    let mut boundaries = Boundaries::default();
+
+    let mut cache: HashMap<Point, u32> = HashMap::new();
+    let mut sum = 1;
+
+    while sum <= aim {
+        cache.insert(point, sum);
+        point = next_step(&point, &mut direction, &mut boundaries);
+        sum = sum_neighbours(point, &cache);
+    }
+
+    sum
+}
+
+fn sum_neighbours(point: Point, cache: &HashMap<Point, u32>) -> u32 {
+    cache.get(&point.left()).unwrap_or(&0)
+        + cache.get(&point.top_left()).unwrap_or(&0)
+        + cache.get(&point.top()).unwrap_or(&0)
+        + cache.get(&point.top_right()).unwrap_or(&0)
+        + cache.get(&point.right()).unwrap_or(&0)
+        + cache.get(&point.bottom_right()).unwrap_or(&0)
+        + cache.get(&point.bottom()).unwrap_or(&0)
+        + cache.get(&point.bottom_left()).unwrap_or(&0)
+}
+
+fn next_step(point: &Point, direction: &mut Direction, boundaries: &mut Boundaries) -> Point {
     match *direction {
         Direction::Left => {
             if point.x == boundaries.min.x {
-                boundaries.min.x -= 1;
+                boundaries.min = boundaries.min.left();
                 *direction = Direction::Down;
             }
-            point.x -= 1;
+            point.left()
         }
         Direction::Right => {
             if point.x == boundaries.max.x {
-                boundaries.max.x += 1;
+                boundaries.max = boundaries.max.right();
                 *direction = Direction::Up;
             }
-            point.x += 1;
+            point.right()
         }
         Direction::Down => {
             if point.y == boundaries.max.y {
-                boundaries.max.y += 1;
+                boundaries.max = boundaries.max.bottom();
                 *direction = Direction::Right;
             }
-            point.y += 1;
+            point.bottom()
         }
         Direction::Up => {
             if point.y == boundaries.min.y {
-                boundaries.min.y -= 1;
+                boundaries.min = boundaries.min.top();
                 *direction = Direction::Left;
             }
-            point.y -= 1;
+            point.top()
         }
     }
 }
@@ -65,10 +96,61 @@ struct Boundaries {
     pub max: Point,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Hash, Eq, PartialEq, PartialOrd, Ord, Clone, Copy)]
 struct Point {
     pub x: i32,
     pub y: i32,
+}
+
+impl Point {
+    fn left(&self) -> Self {
+        Self {
+            x: self.x - 1,
+            y: self.y,
+        }
+    }
+    fn right(&self) -> Self {
+        Self {
+            x: self.x + 1,
+            y: self.y,
+        }
+    }
+    fn top(&self) -> Self {
+        Self {
+            x: self.x,
+            y: self.y - 1,
+        }
+    }
+    fn top_left(&self) -> Self {
+        Self {
+            x: self.x - 1,
+            y: self.y - 1,
+        }
+    }
+    fn top_right(&self) -> Self {
+        Self {
+            x: self.x + 1,
+            y: self.y - 1,
+        }
+    }
+    fn bottom(&self) -> Self {
+        Self {
+            x: self.x,
+            y: self.y + 1,
+        }
+    }
+    fn bottom_left(&self) -> Self {
+        Self {
+            x: self.x - 1,
+            y: self.y + 1,
+        }
+    }
+    fn bottom_right(&self) -> Self {
+        Self {
+            x: self.x + 1,
+            y: self.y + 1,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -81,5 +163,14 @@ mod tests {
         assert_eq!(part_one(12), 3);
         assert_eq!(part_one(23), 2);
         assert_eq!(part_one(1024), 31);
+    }
+
+    #[test]
+    fn part_two_works() {
+        assert_eq!(part_two(1), 2);
+        assert_eq!(part_two(3), 4);
+        assert_eq!(part_two(12), 23);
+        assert_eq!(part_two(23), 25);
+        assert_eq!(part_two(747), 806);
     }
 }
