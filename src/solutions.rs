4c79f308a9af30a9cd58c7e795442926a6505fed
Manuel Krebs <me@leun4m.de>
2022-12-01 13:56:43 +0100

    2022 / Day 01

--- src/solutions.rs
+++ src/solutions.rs
@@ -7,6 +7,7 @@ mod year_2018;
 mod year_2019;
 mod year_2020;
 mod year_2021;
+mod year_2022;
 
 pub fn solve(year: u16, day: u8, input: &str) {
     match year {
@@ -17,6 +18,7 @@ pub fn solve(year: u16, day: u8, input: &str) {
         19 | 2019 => year_2019::solve_day(day, input),
         20 | 2020 => year_2020::solve_day(day, input),
         21 | 2021 => year_2021::solve_day(day, input),
+        22 | 2022 => year_2022::solve_day(day, input),
         _ => println!("{}", util::INVALID_YEAR),
     }
 }
---
+++ src/solutions/year_2022/day_01.rs
@@ -0,0 +1,75 @@
+use itertools::Itertools;
+
+use crate::parser;
+
+pub fn solve(input: &str) {
+    let calories = parse(input).iter().map(|x| x.iter().sum()).collect_vec();
+    println!("Part 1: {}", part_one(&calories));
+    println!("Part 2: {}", part_two(&calories));
+}
+
+type Calories = u32;
+
+fn parse(input: &str) -> Vec<Vec<Calories>> {
+    input
+        .split("\n\n")
+        .map(|group| parser::lines_as_numbers(group))
+        .collect_vec()
+}
+
+fn part_one(input: &[Calories]) -> Calories {
+    input.iter().sorted().rev().take(1).sum()
+}
+
+fn part_two(input: &[Calories]) -> Calories {
+    input.iter().sorted().rev().take(3).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+    use super::*;
+
+    #[test]
+    fn parse_works() {
+        let input = "
+        1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+";
+
+let output = vec![
+    vec![1000, 2000, 3000],
+    vec![4000],
+    vec![5000, 6000],
+    vec![7000, 8000, 9000],
+    vec![10000],
+];
+
+        assert_eq!(parse(input), output);
+    }
+
+    #[test]
+    fn part_one_works() {
+        let input = vec![6000, 4000, 11000, 24000, 10000];
+        assert_eq!(part_one(&input), 24000);
+    }
+    
+    #[test]
+    fn part_two_works() {
+        let input = vec![6000, 4000, 11000, 24000, 10000];
+        assert_eq!(part_two(&input), 45000);
+    }
+}
