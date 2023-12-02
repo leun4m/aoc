999caa95d3a5d5d40682b656b9e0467f8c4e8126
Manuel Krebs <me@leun4m.de>
2023-12-03 00:25:13 +0100

    2023 / Day 01 (Part 2)

--- src/solutions/year_2023/day_01.rs
+++ src/solutions/year_2023/day_01.rs
@@ -1,24 +1,30 @@
 pub fn solve(input: &str) {
-    let parsed = parse(input);
-    println!("Part 1: {}", part_one(&parsed));
+    println!("Part 1: {}", part_one(input));
+    println!("Part 2: {}", part_two(input));
 }
 
-fn parse(input: &str) -> Vec<Vec<u32>> {
-    input.lines().map(|x| parse_numbers(x)).collect()
+const DIGITS: [(&str, u32); 9] = [
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
+
+fn part_one(input: &str) -> u32 {
+    calculate_sum(&parse(input))
 }
 
-fn parse_numbers(input: &str) -> Vec<u32> {
-    input
-        .chars()
-        .filter(|x| x.is_digit(10))
-        .map(|x| x.to_digit(10))
-        .filter(|x| x.is_some())
-        .map(|x| x.unwrap())
-        .collect()
+fn part_two(input: &str) -> u32 {
+    calculate_sum(&parse(&pre_parse(input)))
 }
 
-fn part_one(input: &[Vec<u32>]) -> u32 {
-    input
+fn calculate_sum(numbers: &[Vec<u32>]) -> u32 {
+    numbers
         .iter()
         .map(|x| {
             String::from_iter([
@@ -30,14 +36,68 @@ fn part_one(input: &[Vec<u32>]) -> u32 {
         .sum()
 }
 
+fn parse(input: &str) -> Vec<Vec<u32>> {
+    input.lines().map(parse_line).collect()
+}
+
+fn parse_line(input: &str) -> Vec<u32> {
+    input
+        .chars()
+        .filter(char::is_ascii_digit)
+        .filter_map(|x| x.to_digit(10))
+        .collect()
+}
+
+fn pre_parse(input: &str) -> String {
+    input.lines().map(pre_parse_line).collect()
+}
+
+fn pre_parse_line(input: &str) -> String {
+    let mut i = 0;
+    let mut new_line = input.to_string();
+
+    while i < new_line.len() {
+        let mut temp = new_line[..i].to_string();
+        let mut replace = new_line[i..].to_string();
+
+        if replace_first_digit(&mut replace) {
+            i += 1;
+        }
+
+        temp.push_str(&replace);
+        new_line = temp;
+
+        i += 1;
+    }
+
+    new_line.push('\n');
+    new_line
+}
+
+fn replace_first_digit(input: &mut String) -> bool {
+    for (digit_spelled, digit_value) in DIGITS {
+        if input.starts_with(digit_spelled) {
+            let mut replacement = digit_value.to_string();
+            replacement.push_str(digit_spelled);
+
+            let temp = input.replacen(digit_spelled, &replacement, 1);
+            *input = temp;
+
+            return true;
+        }
+    }
+
+    false
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     const EXAMPLE_A: &str = "1abc2
-    pqr3stu8vwx
-    a1b2c3d4e5f
-    treb7uchet";
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
 
     const EXAMPLE_B: &str = "two1nine
 eightwothree
@@ -57,8 +117,17 @@ zoneight234
 
     #[test]
     fn test_part_one() {
-        assert_eq!(142, part_one(&parse(EXAMPLE_A)));
+        assert_eq!(142, part_one(EXAMPLE_A));
     }
 
+    #[test]
+    fn test_part_two() {
+        assert_eq!(281, part_two(EXAMPLE_B));
+    }
 
+    #[test]
+    fn test_replace_nums() {
+        assert_eq!("1one\n", pre_parse_line("one"));
+        assert_eq!("8eigh2two\n", pre_parse_line("eightwo"));
+    }
 }
