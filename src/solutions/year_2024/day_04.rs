0c6ddd4d9ebfd1f3d47ef20cde4d1225afa4e2ce
Manuel Krebs <me@leun4m.de>
2024-12-05 18:07:44 +0100

    2024 / Day 04 (Part 2) WIP

--- src/solutions/year_2024/day_04.rs
+++ src/solutions/year_2024/day_04.rs
@@ -3,6 +3,7 @@ use crate::parser;
 pub fn solve(input: &str) {
     let matrix = parse(input);
     println!("Part 1: {}", part_one(&matrix));
+    println!("Part 2: {}", part_two(&matrix));
 }
 
 struct Matrix {
@@ -68,6 +69,159 @@ fn part_one(matrix: &Matrix) -> usize {
     }
 
     count
+}
+
+fn part_two(matrix: &Matrix) -> usize {
+    let mut axis = Vec::new();
+
+    let mut findings = Vec::new();
+    // count horizontal -
+    for y in 0..matrix.size {
+        let mut state = LookingForState::Start;
+        let mut pos_a = (0, 0);
+        for x in 0..matrix.size {
+            let c = matrix.grid[y][x];
+
+            state = apply_state(state, c, &mut pos_a, x, y, &mut findings);
+        }
+    }
+    axis.push(findings.clone());
+
+    // count vertical |
+    findings.clear();
+    for x in 0..matrix.size {
+        let mut state = LookingForState::Start;
+        let mut pos_a = (0, 0);
+        for y in 0..matrix.size {
+            let c = matrix.grid[y][x];
+
+            state = apply_state(state, c, &mut pos_a, x, y, &mut findings);
+        }
+    }
+    axis.push(findings.clone());
+
+    // count diagonal /
+    findings.clear();
+    for x in 0..matrix.size {
+        let mut state = LookingForState::Start;
+        let mut pos_a = (0, 0);
+        for i in 0..matrix.size - x {
+            let dx = x + i;
+            let dy = i;
+            let c = matrix.grid[dy][dx];
+            state = apply_state(state, c, &mut pos_a, dx, dy, &mut findings);
+        }
+    }
+    for y in 1..matrix.size {
+        let mut state = LookingForState::Start;
+        let mut pos_a = (0, 0);
+        for i in 0..matrix.size - y {
+            let dx = i;
+            let dy = y + i;
+            let c = matrix.grid[dy][dx];
+            state = apply_state(state, c, &mut pos_a, dx, dy, &mut findings);
+        }
+    }
+    axis.push(findings.clone());
+
+    // count diagonal \
+    findings.clear();
+    for x in 0..matrix.size {
+        let mut state = LookingForState::Start;
+        let mut pos_a = (0, 0);
+        for i in 0..matrix.size - x {
+            let dx = x + i;
+            let dy = matrix.size - 1 - i;
+            let c = matrix.grid[dy][dx];
+            state = apply_state(state, c, &mut pos_a, dx, dy, &mut findings);
+        }
+    }
+    for y in 1..matrix.size {
+        let mut state = LookingForState::Start;
+        let mut pos_a = (0, 0);
+        for i in 0..matrix.size - y {
+            let dx = i;
+            let dy = matrix.size - 1 - y - i;
+            let c = matrix.grid[dy][dx];
+            state = apply_state(state, c, &mut pos_a, dx, dy, &mut findings);
+        }
+    }
+    axis.push(findings.clone());
+
+    count_duplicates_on_axis(axis)
+}
+
+fn apply_state(
+    state: LookingForState,
+    c: char,
+    position_of_a: &mut (usize, usize),
+    x: usize,
+    y: usize,
+    position: &mut Vec<(usize, usize)>,
+) -> LookingForState {
+    match state {
+        LookingForState::Start => {
+            if c == 'M' {
+                LookingForState::MA
+            } else if c == 'S' {
+                LookingForState::SA
+            } else {
+                LookingForState::Start
+            }
+        }
+        LookingForState::MA => {
+            if c == 'A' {
+                *position_of_a = (x, y);
+                LookingForState::MAS
+            } else {
+                LookingForState::Start
+            }
+        }
+        LookingForState::SA => {
+            if c == 'A' {
+                *position_of_a = (x, y);
+                LookingForState::SAM
+            } else {
+                LookingForState::Start
+            }
+        }
+        LookingForState::MAS => {
+            if c == 'S' {
+                position.push(*position_of_a);
+            }
+            LookingForState::Start
+        }
+        LookingForState::SAM => {
+            if c == 'M' {
+                position.push(*position_of_a);
+            }
+            LookingForState::Start
+        }
+    }
+}
+
+#[allow(clippy::upper_case_acronyms)]
+#[derive(Debug, PartialEq, Eq)]
+enum LookingForState {
+    Start,
+    MA,
+    SA,
+    MAS,
+    SAM,
+}
+
+fn count_duplicates_on_axis(axis: Vec<Vec<(usize, usize)>>) -> usize {
+    let mut count = 0;
+    for i in 0..axis.len() {
+        for k in i + 1..axis.len() {
+            count += find_duplicates(&axis[i], &axis[k]);
+        }
+    }
+    count
+}
+
+fn find_duplicates(a: &[(usize, usize)], b: &[(usize, usize)]) -> usize {
+    a.iter().filter(|p| b.contains(p)).count()
 }
 
 const SEARCH_WORD: &str = "XMAS";
@@ -143,5 +297,24 @@ MXMXAXMASX";
     fn test_part_one() {
         let matrix = parse(EXAMPLE_INPUT);
         assert_eq!(part_one(&matrix), 18);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let matrix = parse(EXAMPLE_INPUT);
+        assert_eq!(part_two(&matrix), 9);
+    }
+
+    #[test]
+    fn test_find_duplicates() {
+        assert_eq!(find_duplicates(&[], &[]), 0);
+        assert_eq!(find_duplicates(&[(1, 1)], &[]), 0);
+        assert_eq!(find_duplicates(&[], &[(1, 1)]), 0);
+        assert_eq!(find_duplicates(&[(1, 1)], &[(1, 1)]), 1);
+        assert_eq!(find_duplicates(&[(1, 1)], &[(2, 2)]), 0);
+        assert_eq!(
+            find_duplicates(&[(1, 1), (2, 2), (3, 3)], &[(2, 2), (3, 3)]),
+            2
+        );
     }
 }
